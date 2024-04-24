602d164d50bee4dfe765548a49bcdf0c221bbb85
carlos borrego <ingcarlosborrego@gmail.com>
2024-04-23 21:55:54 -0500

    Primer commit; index.html listo y el css completo

---
+++ index.js
@@ -0,0 +1,11 @@
+//Html Elemnts
+const board = document.getElementById("board");
+const scoreBoard = document.getElementById("scoreBoard");
+const startButton = document.getElementById("start");
+const gameOverSign = document.getElementById("gameOver");
+
+//Game settings
+const boardSize = 10;
+const gameSpeed = 100;
+
+//
